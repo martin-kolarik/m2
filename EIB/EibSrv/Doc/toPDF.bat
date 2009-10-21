4740f07b3d71e765d5900bbe9d06a6bc2aadeb0a
martin.kolarik@smartcontrol.cz <martin.kolarik@smartcontrol.cz>
2009-10-21 21:35:53 +0200

    Issue #142 Kromě kořene vše přejmenováno a vyzkoušeno.
    --HG--
    branch : EibSrv.1.x

--- EIB/EibSrv/Doc/toPDF.bat
+++ EIB/EibSrv/Doc/toPDF.bat
@@ -1,3 +1,3 @@
 call d:\tex\setup
-call mk-xsltex sty-i-scdoc eibsrv_en.xml "--mode=A5"
-call mk-xsltex sty-i-scdoc eibsrv_cs.xml "--mode=A5"
+call mk-xsltex sty-i-scdoc smartserver_en.xml "--mode=A5"
+call mk-xsltex sty-i-scdoc smartserver_cs.xml "--mode=A5"
