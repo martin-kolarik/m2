8a5d1af136efe42cb7efe4ef75cb3ac32d7f87b5
martin.kolarik@smartcontrol.cz <martin.kolarik@smartcontrol.cz>
2009-11-21 22:04:22 +0100

    Přejmenování na C Compiler Support Functions.

---
+++ API/CCSuppFunc/CRtl/Source/coninit.c
@@ -0,0 +1,39 @@
+/* entry points/exe initialization */
+
+#pragma once
+
+#include "initterm.h"
+
+/* from Win32 API */
+extern unsigned short** __stdcall CommandLineToArgvW( unsigned short* lpCmdLine, int* pNumArgs );
+extern unsigned short* __stdcall GetCommandLineW();
+extern void* __stdcall LocalFree( void* hMem );
+extern void __stdcall ExitProcess( unsigned int exitCode );
+
+/* Main is a startup routine of program */
+extern int __cdecl Main( int argc, unsigned short** argv ); 
+
+int __cdecl mainCRTStartup()
+{
+    int ret = 0;
+    int argc = 0;
+    unsigned short** argv = 0;
+
+    __initcrtl();
+    argv = CommandLineToArgvW( GetCommandLineW(), &argc );
+    if( argv == 0 )
+    {
+        argc = 0;
+    }
+
+	ret = Main( argc, argv );
+    
+    if( argv != 0 )
+    {
+        LocalFree( argv );
+    }
+    __donecrtl();
+
+    ExitProcess( ret );
+    return ret;
+}
