db657d6bc918ee1e0fb72768c86d5f300d919831
Martin@Turms <Martin@Turms>
2011-09-28 20:46:36 +0200

    Oprava zmateného INSIDE
    --HG--
    branch : EibSrv.2.x

--- Modula2/modula2/Header/m2cpp.h
+++ Modula2/modula2/Header/m2cpp.h
@@ -307,12 +307,12 @@ inline BOOLEAN EQUALSM_(const BYTE* S1, const BYTE* S2, CARDINAL L) throw()
 // strings -- INSIDE ANSI
 inline BOOLEAN INSIDEB_(INTEGER HIGH_, const CHAR* S, ORDINAL I) throw()
 {
-  return I <= HIGH_ && S[I] != 0;
+  return S != 0 && I <= HIGH_;
 }
 // strings -- INSIDE UNICODE
 inline BOOLEAN INSIDEW_(INTEGER HIGH_, const WCHAR* S, ORDINAL I) throw()
 {
-  return I <= HIGH_ && S[I] != 0;
+  return S != 0 && I <= HIGH_;
 }
 
 // strings -- LENGTH ANSI
