b2ab2e83d3779450afd9080cd18227ef8c4aa6f2
martin.kolarik@email.cz <martin.kolarik@email.cz>
2014-01-28 23:59:58 +0100

    přidán SimulatorFiller, otestováno počítání vzdáleností, otestováno plnění

---
+++ PhD/TemplateOptimizer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateOptimizer
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Weights weights = new Weights( 100 );
+            for( var i = 0; i < weights.ComponentCount; i++ )
+            {
+                weights[i] = 1.0;
+            }
+
+            Space space = new Space();
+            space.AddTemplate( new Template( new double[] { 0, 0 } ) );
+            space.AddTemplate( new Template( new double[] { 3, 0 } ) );
+            space.AddTemplate( new Template( new double[] { 3, 4 } ) );
+
+            var dS = space.Distance( Space.DistanceType.Summation, weights );
+            var dA = space.Distance( Space.DistanceType.Average, weights );
+            var dG = space.Distance( Space.DistanceType.GeometricAverage, weights );
+
+            SimulationFiller filler = new SimulationFiller( 100, 100,
+                (component) =>
+                {
+                    switch( component )
+                    {
+                        default:
+                            return SimulationFiller.ComponentType.RandomNormal;
+                    }
+                },
+                (component) =>
+                {
+                    switch( component )
+                    {
+                        default:
+                            return new Tuple<double, double>( 0, 0.1 );
+                    }
+                }
+            );
+            space = filler.Populate();
+
+            dS = space.Distance( Space.DistanceType.Summation, weights );
+            dA = space.Distance( Space.DistanceType.Average, weights );
+            dG = space.Distance( Space.DistanceType.GeometricAverage, weights );
+        }
+    }
+}
---
+++ PhD/TemplateOptimizer/SimulationFiller.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Troschuetz.Random;
+
+namespace TemplateOptimizer
+{
+    class SimulationFiller
+    {
+        public enum ComponentType
+        {
+            Deterministic,
+            RandomNormal,
+            RandomPearson,
+            RandomUniform,
+            RandomWeibull
+        }
+
+        private MT19937Generator rng;
+        private object[] sources;
+        private int templateCount;
+        private int componentCount;
+
+        // Tuple<double, double> contains:
+        // -- mi, sigma for RandomNormal
+        // -- c, lambda for Weibull
+        // -- a, b for Uniform
+        // -- n for Pearson
+        // -- y for Deterministic
+        public SimulationFiller( int templateCount, int templateComponentCount, Func<int, ComponentType> componentType, Func<int, Tuple<double, double>> componentParameter, Func<int /* template */, int /* component */, double> componentValue = null)
+        {
+            this.templateCount = templateCount;
+            this.componentCount = templateComponentCount;
+
+            rng = new MT19937Generator( (uint)DateTime.Now.Ticks );
+            sources = new object[templateComponentCount];
+            for( var i = 0; i < templateComponentCount; i++ )
+            {
+                if( componentValue == null )
+                {
+                    var parameters = componentParameter( i );
+                    Distribution distribution;
+                    switch( componentType( i ) )
+                    {
+                        case ComponentType.Deterministic:
+                            throw new Exception( "Unexpected component type, Deterministic must specify 'componentValue' member" );
+                        case ComponentType.RandomNormal:
+                            distribution = new NormalDistribution( rng ) { Mu = parameters.Item1, Sigma = parameters.Item2 };
+                            break;
+                        case ComponentType.RandomPearson:
+                            distribution = new ChiSquareDistribution( rng ) { Alpha = (int)parameters.Item1 };
+                            break;
+                        case ComponentType.RandomUniform:
+                            distribution = new ContinuousUniformDistribution( rng ) { Alpha = parameters.Item1, Beta = parameters.Item2 };
+                            break;
+                        case ComponentType.RandomWeibull:
+                            distribution = new WeibullDistribution( rng ) { Alpha = parameters.Item1, Lambda = parameters.Item2 };
+                            break;
+                        default:
+                            throw new Exception( "Unknown component type" );
+                    }
+                    sources[i] = distribution;
+                }
+                else
+                {
+                    sources[i] = componentValue;
+                }
+            }
+        }
+
+        public Space Populate()
+        {
+            var space = new Space();
+
+            for( var i = 0; i < templateCount; i++ )
+            {
+                var template = new Template( componentCount );
+
+                for ( var j = 0; j < componentCount; j++ )
+                {
+                    if( sources[j] is Distribution )
+                    {
+                        template[j] = ( (Distribution)sources[j] ).NextDouble();
+                    }
+                    else
+                    {
+                        template[j] = ( (Func<int, int, double>)sources[j] )( i, j );
+                    }
+                }
+
+                space.AddTemplate( template );
+            }
+
+            return space;
+        }
+    }
+}
